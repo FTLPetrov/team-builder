c803546bcab9bab3cb9e80205aaf57075e50fa13
petyr.lubomirov.petrov@gmail.com <petyr.lubomirov.petrov@gmail.com>
2025-07-30 13:12:22 +0300

    client updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
-import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { AppLayout } from './components/layout';
+import { AppRoutes } from './components/routing';
+import './index.css';
 
 function App() {
-    return (
-    <>      
-    </>
-  )
+  return (
+    <AuthProvider>
+      <Router>
+        <AppLayout>
+          <AppRoutes />
+        </AppLayout>
+      </Router>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
